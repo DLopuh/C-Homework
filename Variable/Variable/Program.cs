3620cf8383e959ad2b6404e6bba510f6c5a59016
DLopuhin <dmitriy.lopukhin@icloud.com>
2024-04-05 21:48:17 +0300

    first

--- Variable/Variable/Program.cs
+++ Variable/Variable/Program.cs
@@ -10,7 +10,38 @@ namespace Variable
     {
         static void Main(string[] args)
         {
+            string fullName = "Лопухин Дмитрий Михайлович";
 
+            int age = 38;
+
+            string email = "dmitriy.lopukhin@icloud.com";
+
+            float programmingPoint = 99.8f;
+
+            double mathScore = 99.8;
+
+            decimal phisicsPoints = 99.8M;
+
+            Console.WriteLine(fullName);
+            Console.WriteLine(age);
+            Console.WriteLine(email);
+            Console.WriteLine(programmingPoint);
+            Console.WriteLine(mathScore);
+            Console.WriteLine(phisicsPoints);
+
+            Console.ReadLine();
+
+            decimal sum = (decimal)programmingPoint + (decimal)mathScore + phisicsPoints;
+
+            Console.WriteLine(sum);
+
+            Console.ReadLine();
+
+            decimal averageMark = ((programmingPoint + mathScore + phisicsPoints) / 3);
+
+            Console.WriteLine(averageMark);
+
+            Console.ReadLine();
         }
     }
 }
